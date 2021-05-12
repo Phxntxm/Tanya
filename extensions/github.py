e726c8347e906af09820566f5c0e68186e820f57
Dan Hess <phxntxm1@gmail.com>
2021-05-11 18:01:59 -0800

    Make sure to pull first

--- extensions/github.py
+++ extensions/github.py
@@ -18,7 +18,11 @@ class Github(commands.Cog):
 
     async def receive_push(self, request):
         response = await request.json()
-
+        # First pull from github
+        await asyncio.create_subprocess_shell(
+            "git pull", stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT
+        )
+        # Now load/reload/unload based on added/removed/modified
         loaded = []
         reloaded = []
         unloaded = []
