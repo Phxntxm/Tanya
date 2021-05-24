43fac4ef59c3f2c1e7835aea1bc134315eeb2516
IAmTomahawkx <iamtomahawkx@gmail.com>
2021-05-23 19:31:48 -0700

    fix conflict

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import multiprocessing
-import types
 import typing
 from glob import glob
 
+import asyncpg
 import discord
 from discord.ext import commands
 
 import config
-from utils import Cog, Context
+from utils import Context
 
 intents = discord.Intents(
     guild_messages=True,
@@ -26,9 +26,16 @@ class MafiaBot(commands.Bot):
                 command_attrs={"name": "commands", "aliases": ["command"]}
             ),
         )
+        self.db: typing.Optional[asyncpg.pool.Pool] = None
 
     async def get_context(self, message, *, cls=Context):
         return await super().get_context(message, cls=cls)
+
+    async def start(
+        self, token: str, *, bot: bool = ..., reconnect: bool = ...
+    ) -> None:
+        self.db = await asyncpg.create_pool(config.db_uri, min_size=1, max_inactive_connection_lifetime=10)
+        return await super().start(token, bot=bot, reconnect=reconnect)
 
 
 bot = MafiaBot()
@@ -37,5 +44,8 @@ if __name__ == "__main__":
     multiprocessing.set_start_method("forkserver")
     for ext in glob("extensions/*.py"):
         bot.load_extension(ext.replace("/", ".")[:-3])
+
+    if getattr(config, "fuck_you_sarc"):
+        bot.load_extension("jishaku")
 
     bot.run(config.token)
