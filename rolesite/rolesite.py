2dfda36910a1fd6399714a018d41b4f83be0437a
Dan Hess <phxntxm1@gmail.com>
2021-05-26 15:55:56 -0800

    Add web dashboard for roles

---
+++ rolesite/rolesite.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def serve_site():
+    roles = [
+        ("Mafia", 50),
+        ("Citizen", 51),
+        ("Sheriff", 52),
+        ("Jester", 53),
+        ("Executior", 54),
+        ("Survivor", 55),
+        ("Doctor", 56),
+        ("PI", 57),
+        ("Lookout", 58),
+        ("Jailor", 59),
+    ]
+    return render_template("index.html", roles=roles)
+
+
+if __name__ == "__main__":
+    app.run()
