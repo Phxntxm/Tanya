958adb957bfd22fa91b0a6e8508daee335ebfd34
IAmTomahawkx <iamtomahawkx@gmail.com>
2021-05-27 18:20:28 -0700

    collections.Counter moment 2

--- extensions/stats.py
+++ extensions/stats.py
@@ -75,7 +75,7 @@ class Stats(commands.Cog):
             f"{'You have' if user == ctx.author else f'{user} has'} played {len(games)} games{' in this server' if only_this_server else ''}, "
             f"won {wins} games, killed {kills-suicides} people, died {deaths} times, committed suicide {suicides} times, and "
             f"been mafia {mafia} times.\n\n{'Your' if user == ctx.author else f'{user.name}{apost}s'} most common role "
-            f"{'here ' if only_this_server else ''}is {top_role}"
+            f"{'here ' if only_this_server else ''}is {top_role[0]}, with {top_role[1]} games."
         )
         await ctx.reply(fmt, mention_author=False)
 
