ccb29f65aecf2a65f5025719f4f2dc945579d130
Dan Hess <phxntxm1@gmail.com>
2021-05-29 17:46:30 -0800

    Optimize stats query

--- extensions/stats.py
+++ extensions/stats.py
@@ -1,17 +1,12 @@
 import collections
 import typing
 
-import asyncpg
 import discord
 from discord.ext import commands
 from utils import Context
 
 if typing.TYPE_CHECKING:
     from utils.custom_bot import MafiaBot
-
-
-def condition(predicate: typing.Callable[[asyncpg.Record], bool], data: list) -> int:
-    return len(tuple(filter(predicate, data)))
 
 
 class Stats(commands.Cog):
@@ -38,52 +33,75 @@ class Stats(commands.Cog):
 
         query = """
         SELECT
-            games.id, guild_id, day_count, p.win, p.die, r.name AS role, r.alignment
-        FROM games
-        INNER JOIN players p ON
-            games.id = p.game_id AND p.user_id = $1
-        INNER JOIN roles r ON
-            r.id = p.role
-        WHERE
-            user_id = $1
+            games.id,
+            players.user_id,
+            roles.name,
+            roles.alignment,
+            players.win,
+            players.die,
+            killed.killer,
+            killed.suicide AS died_by_suicide,
+            killed.lynch AS died_by_lynch,
+            kills.killed
+        FROM
+            players
+        LEFT JOIN games
+            ON games.id = players.game_id
+        LEFT JOIN roles
+            ON roles.id = players.role
+        LEFT JOIN kills killed
+            ON killed.killed = players.user_id AND killed.game_id = games.id
+        LEFT JOIN kills
+            ON kills.killer = players.user_id AND kills.game_id = games.id
+        WHERE players.user_id = $1
         """
-        async with ctx.acquire() as conn:
-            games = await conn.fetch(query, _user.id)
-
-            query = """
-            SELECT
-                game_id, killer, killed, night, suicide
-            FROM kills
-            WHERE
-                killer = $1 OR killed = $1
-            """
-            kills = await conn.fetch(query, _user.id)
-
-        if not games:
-            return await ctx.reply(f"No stats for {_user}", mention_author=False)
 
         if only_this_server:
-            games = list(filter(lambda row: row["guild_id"] == ctx.guild.id, games))
-            _game_ids = tuple(x["id"] for x in games)
-            kills = list(filter(lambda row: row["game_id"] in _game_ids, kills))
+            query += " AND guild_id = $2"
+            args = (_user.id, ctx.guild.id)
+        else:
+            args = (_user.id,)
 
-        wins = condition(lambda row: row["win"], games)
-        suicides = condition(lambda row: row["suicide"], kills)
-        mafia = condition(lambda row: row["role"] == "Mafia", games)
-        roles = collections.Counter([x["role"] for x in games])
+        async with ctx.acquire() as conn:
+            player = await conn.fetch(query, *args)
+
+        if not player:
+            return await ctx.reply(f"No stats for {_user}", mention_author=False)
+
+        games = set()
+        wins = suicides = lynches = mafia = citizen = independent = kills = deaths = 0
+        roles = collections.Counter()
+
+        for row in player:
+            games.add(row["id"])
+            roles.update(row["name"])
+
+            if row["win"]:
+                wins += 1
+            if row["alignment"] == 1:
+                citizen += 1
+            if row["alignment"] == 2:
+                independent += 1
+            if row["alignment"] == 3:
+                mafia += 1
+            if row["killer"]:
+                deaths += 1
+            if row["killed"]:
+                kills += 1
+            if row["lynch"]:
+                lynches += 1
+            if row["suicide"]:
+                suicides += 1
+
         top_role = roles.most_common(1)[0]
-
-        deaths = condition(lambda row: row["killed"] == _user.id, kills)
-        kills = condition(lambda row: row["killer"] == _user.id, kills)
-
         apost = "'"  # stupid fstrings
 
         fmt = (
             f"{'You have' if _user == ctx.author else f'{_user} has'} played {len(games)} game{'s' if len(games) != 1 else ''}"
             f"{' in this server' if only_this_server else ''}, won {wins} game{'s' if wins != 1 else ''}, "
-            f"killed {kills-suicides} {'people' if kills-suicides != 1 else 'person'}, died {deaths} time{'s' if deaths != 1 else ''}, committed suicide "
-            f"{suicides} time{'s' if suicides != 1 else ''}, and been mafia {mafia} time{'s' if mafia != 1 else ''}.\n\n"
-            f"{'Your' if _user == ctx.author else f'{_user.name}{apost}s'} most common role "
+            f"killed {kills} {'people' if kills != 1 else 'person'}, died {deaths} time{'s' if deaths != 1 else ''}, committed suicide "
+            f"{suicides} time{'s' if suicides != 1 else ''}, been lynched {lynches} time{'s' if lynches != 1 else ''} and been mafia "
+            f"{mafia} time{'s' if mafia != 1 else ''}.\n\n{'Your' if _user == ctx.author else f'{_user.name}{apost}s'} most common role "
             f"{'here ' if only_this_server else ''}is {top_role[0]}, with {top_role[1]} game{'s' if top_role[1] != 1 else ''}."
         )
         await ctx.reply(fmt, mention_author=False)
@@ -146,7 +164,7 @@ class Stats(commands.Cog):
             f"{lynch_count} lynch{'es' if lynch_count != 1 else ''}, "
             f"{mafia_wins} win{'s' if mafia_wins != 1 else ''} by the mafia, "
             f"{ind_wins} win{'s' if ind_wins != 1 else ''} from independents, "
-            f"and {cit_wins} win{'s' if cit_wins != 1 else ''} civilian wins."
+            f"and {cit_wins} win{'s' if cit_wins != 1 else ''} from civilians."
         )
         await ctx.reply(fmt, mention_author=False)
 
